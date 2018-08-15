27a51a89921e6664b3f8e3c39b0f7b29d5b8fc84
Lois Tonnet <tonnetlois@gmail.com>
2018-08-15 22:11:01 +0200

    feat: added command parsing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const Discord = require('discord.js');
 const env = require('../config.json');
+const secrets = require('../secrets.json');
 
 const client = new Discord.Client();
+const prefix = env.prefix;
 
 client.on('ready', () => {
-  console.log('Ready!');
+  console.log('Client is ready!');
 });
 
 client.on('message', message => {
-  console.log(message.content);
+  // Checks if the message has to be processed.
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  // Parses the message using spaces and tabs.
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const commandName = args.shift().toLowerCase();
+
+  console.log(`Command Name: ${commandName}\nArgs: ${args}`);
 });
 
-client.login(env.token);
+client.login(secrets.token);
